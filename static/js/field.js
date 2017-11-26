c042add342d2a58b1c08c2fdfe556289dc9f5ef3
theflyingmantis <rai.1@iitj.ac.in>
2017-11-27 03:18:35 +0530

    beautification

---
+++ static/js/field.js
@@ -0,0 +1,34 @@
+let fields = 1;
+function add_field(){
+  fields++;
+  var i = document.createElement("input");
+  i.type = "text";
+  i.name = "n"+fields.toString();
+  i.id = "n"+fields.toString();
+  i.required = true;
+
+  var li = document.createElement("Label");
+  li.setAttribute("for","n"+fields.toString());
+  li.innerHTML = "Name of field:"
+
+    var j=document.createElement("input");
+  j.type="Number";
+  j.name = "v"+fields.toString();
+  j.id = "v"+fields.toString();
+  j.step = "0.01";
+  j.required = true;
+
+  var lj = document.createElement("Label");
+  lj.setAttribute("for","v"+fields.toString());
+  lj.innerHTML = "Probability of Field:"
+
+
+    let f = document.getElementById('form_data');
+  var br = document.createElement("br");
+  f.appendChild(br);
+  f.appendChild(li);   
+  f.appendChild(i);
+  f.appendChild(lj);     
+  f.appendChild(j);
+  console.log(fields);
+}
